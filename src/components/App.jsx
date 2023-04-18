d56a17e83b7a3b0b963b1b5b07dde4d4fbc2b236
Ivan Panteliuk <i.v.pantelyuk@gmail.com>
2023-04-18 21:41:13 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,7 +20,7 @@ export class App extends Component {
   componentDidMount() {
     const contacts = localStorage.getItem('contacts');
     const parsedContacts = JSON.parse(contacts);
-    console.log(parsedContacts);
+
     if (parsedContacts && parsedContacts.length) {
       this.setState({ contacts: parsedContacts });
     } else {
